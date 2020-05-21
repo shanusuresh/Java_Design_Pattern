37662bd5e0b26a88ad0e90ac0e290878f5385962
shanusuresh <shanmugapriya.sabapathy@gmail.com>
2020-05-21 08:56:28 +0530

    Update GoogleFactory.java
    Update GoogleFactory class

--- src/main/java/com/learn/designpattern/factory/GoogleFactory.java
+++ src/main/java/com/learn/designpattern/factory/GoogleFactory.java
@@ -2,6 +2,8 @@ package com.learn.designpattern.factory;
 
 import org.openqa.selenium.WebDriver;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 public class GoogleFactory {
@@ -9,4 +11,19 @@ public class GoogleFactory {
     private static final Function<WebDriver,GooglePage> ENG = (d)->new GoogleEnglishPage(d);
     private static final Function<WebDriver,GooglePage> TAM = (d)-> new GoogleTamilPage(d);
     private static final Function<WebDriver,GooglePage> HIN = (d)-> new GoogleHindiPage(d);
+
+    private static final Map<String,Function<WebDriver,GooglePage>> MAP = new HashMap<>();
+
+    static{
+        MAP.put("ENG",ENG);
+        MAP.put("TAM",TAM);
+        MAP.put("HIN",HIN);
+    }
+
+    public static GooglePage get(String language, WebDriver driver){
+        return MAP.get(language).apply(driver);
+    }
+
+
+
 }
